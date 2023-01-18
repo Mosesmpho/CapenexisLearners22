a88849910dea891b6c6feccff34dbbd910bd97e3
Mosesmpho <mosesmpho16@gmail.com>
2023-01-18 08:54:53 +0200

    Scaffolded the folders of Learners,Facilitators & CourseName

--- CapenexisLearners22/Program.cs
+++ CapenexisLearners22/Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CapenexisLearners22.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<CapenexisLearners22Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CapenexisLearners22Context") ?? throw new InvalidOperationException("Connection string 'CapenexisLearners22Context' not found.")));
 
 var app = builder.Build();
 
